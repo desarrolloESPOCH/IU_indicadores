9ff8bb95903c1a5ed14bb6990f6e299cff01be96
Yariel <bryan.baldeon@espoch.edu.ec>
2025-05-16 15:34:19 -0500

    (feat): render de los 33 indicadores de calidad

---
+++ src/app/modules/shared/services/calidad.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { IResponse } from '../models/IResponse.interface';
+@Injectable({
+  providedIn: 'root',
+})
+export class CalidadService {
+  getAll = async (
+    idCarrera: string,
+  ): Promise<IResponse<IndicadoresCalidad>> => {
+    const response = await fetch(`${environment.api}/calidad/${idCarrera}`);
+    const data = await response.json();
+    return data;
+  };
+}
+
+export interface Periodo {
+  idValor: number;
+  valor: number;
+  nombrePeriodo: string;
+}
+
+export interface IndicadoresCalidad {
+  titulo: string;
+  idIndicador: number;
+  periodos: Periodo[];
+}
